a14790acd5f3816b167045536e02d37df692d7f9
PrashannaR <prashanna.rajbhandari1@gmail.com>
2022-07-08 00:05:35 +0545

    feat: HomeScreen UI added

--- mobileApp/app/src/main/java/com/prashannar/mitosys/MainActivity.kt
+++ mobileApp/app/src/main/java/com/prashannar/mitosys/MainActivity.kt
@@ -1,14 +1,54 @@
 package com.prashannar.mitosys
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //splash screen
         Thread.sleep(3000)
         val splashScreen = installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        //hide action bar
+        supportActionBar?.hide()
+
+        loginBtn.setOnClickListener {
+            checkInput()
+        }
+    }
+
+    private fun checkInput() {
+        if(emailET.text.toString() != "hehe@gmail.com"){
+            emailET.error = "Invalid email"
+        }else if(passET.text.toString() != "Nepal123"){
+            passET.error = "Invalid Password"
+        }else{
+            val intent = Intent(this, HomeScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        supportActionBar?.hide()
+        super.onStart()
+    }
+
+    override fun onResume() {
+        supportActionBar?.hide()
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        supportActionBar?.hide()
+        super.onRestart()
     }
 }
+
